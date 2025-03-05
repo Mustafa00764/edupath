e276ad6edc6f29a21db849299885fa0531bf476c
Mustafa00764 <eshchanov9@gmail.com>
2025-03-05 15:47:53 +0500

    ll

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,15 +5,10 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
-    // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
-    "*.{js,ts,jsx,tsx,mdx}",
+    "*.{js,ts,jsx,tsx,mdx}", // <-- этот можно убрать, он слишком общий
   ],
+  
   important: true,
   theme: {
       screens: {
